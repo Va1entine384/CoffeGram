6b6452ff798ef61b744fe6ff696d0f85dc6af38e
Valentine384 <Valikverenic@gmail.com>
2025-06-11 10:09:58 +0300

    first

---
+++ src/Components/Login.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { BiCoffeeTogo } from "react-icons/bi";
+import { VscAccount } from "react-icons/vsc";
+import { VscKey } from "react-icons/vsc";
+
+import "../css/Login.css";
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [rememberMe, setRememberMe] = useState(false);
+    const navigate = useNavigate();
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        if (username.trim() && password.trim()) {
+            localStorage.setItem("user", username);
+            navigate("/feed");
+        }
+    };
+
+    return (
+        <div className="login-container">
+            <div className="login-form">
+                <h1 className="app-name">CoffeGram</h1>
+                <div className="icon-wrapper">
+                    <BiCoffeeTogo className="account-icon" />
+                </div>
+                <h2 className="login-title">Вход в аккаунт</h2>
+                <form onSubmit={handleLogin}>
+                    <div className="form-group">
+                        <label htmlFor="username" className="input-label">Имя пользователя</label>
+                        <div className="name-wrapper"> 
+                            <VscAccount className="name-icon" />   
+                            <input
+                                type="text"
+                                id="username"
+                                placeholder="Введите ваше имя пользователя"
+                                style={{paddingLeft: '35px', }}
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                                className="login-input"
+                                required
+                            />
+                        </div>
+                    </div>
+                    
+                    <div className="form-group">
+                        <label htmlFor="password" className="input-label">Пароль</label>
+                        <div className="input-wrapper">
+                            <VscKey className="password-icon" />
+                            <input
+                                type="password"
+                                id="password"
+                                placeholder="Введите ваш пароль"
+                                style={{paddingLeft: '35px',
+                                }}
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                className="login-input"
+                                required
+                            />
+                        </div>
+                    </div>
+                    
+                    <div className="form-options">
+                        <label className="remember-me">
+                            <input
+                                type="checkbox"
+                                checked={rememberMe}
+                                onChange={(e) => setRememberMe(e.target.checked)}
+                            />
+                            <span>Запомнить меня</span>
+                        </label>
+                        {/* <a href="/forgot-password" className="forgot-password">Forgot Password?</a> */}
+                    </div>
+                    
+                    <button type="submit" className="login-button">Войти</button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
