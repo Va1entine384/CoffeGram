5e03eb5b511f4d7e8c525b2e75d05044fb927380
Valentine384 <Valikverenic@gmail.com>
2025-06-13 11:16:17 +0300

    Adde viewing existing users and hashing password.

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { CryptoJS } from "crypto-js";
 import { BiCoffeeTogo } from "react-icons/bi";
 import { VscAccount } from "react-icons/vsc";
 import { VscKey } from "react-icons/vsc";
-
+import { BiGroup } from "react-icons/bi";
 import "../css/Login.css";
 
 const Login = () => {
@@ -15,7 +16,20 @@ const Login = () => {
     const handleLogin = (e) => {
         e.preventDefault();
         if (username.trim() && password.trim()) {
+            const hashedPassword = CryptoJS.SHA256(password).toString();
+            
             localStorage.setItem("user", username);
+            localStorage.setItem("password", hashedPassword);
+
+            const users = JSON.parse(localStorage.getItem("users")) || [];
+
+            const userExists = users.some(user => user.username ===username);
+
+            if(!userExists) {
+                users.push({username, passwordHash: hashedPassword});
+                localStorage.setItem("users", JSON.stringify(users))
+            }
+
             navigate("/feed");
         }
     };
@@ -23,6 +37,9 @@ const Login = () => {
     return (
         <div className="login-container">
             <div className="login-form">
+                <div className="exist-users">
+                    <BiGroup className="real-users" alt = "existedUsers" onClick={() => navigate("/users")}/>
+                </div>
                 <h1 className="app-name">CoffeGram</h1>
                 <div className="icon-wrapper">
                     <BiCoffeeTogo className="account-icon" />
