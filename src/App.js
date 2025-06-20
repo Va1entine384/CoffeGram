46c5ba671619d317132b4a36f92d85b973442e8f
Valentine384 <Valikverenic@gmail.com>
2025-06-20 16:36:00 +0300

    First version(experimental) of realizing chat in CoffeGram

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from "react";
 import { Routes, Route} from "react-router-dom";
 import Login from "./Components/Login.jsx";
 import Feed from "./Components/Feed.jsx";
+import Chat from "./Components/Chat.jsx";
 
 function App () {
   return (
     <Routes>
       <Route path="/" element={<Login />}/>
       <Route path="/feed" element={<Feed />}/>
+      <Route path="/chat" element={<Chat currentUser={localStorage.getItem("user")} />} />
     </Routes>
   )
 }
--- src/Components/FeedComponents/SideNav.jsx
+++ src/Components/FeedComponents/SideNav.jsx
@@ -1,8 +1,15 @@
 import { MdAddToPhotos } from "react-icons/md";
 import { BsChatDotsFill } from "react-icons/bs";
 import { IoSunny } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 
 const SideNav = ({ isEditing, currentUser, onAddPost }) => {
+  const navigate = useNavigate();
+
+  const handleChatClick = () => {
+    navigate("/chat");
+  };
+
   return (
     <div className="sideNav">
       <div className="navIcons">
@@ -13,7 +20,7 @@ const SideNav = ({ isEditing, currentUser, onAddPost }) => {
             title="Добавить пост"
           />
         )}
-        <BsChatDotsFill className="navIcon" title="Чат" />
+        <BsChatDotsFill className="navIcon" title="Чат" onClick={handleChatClick}  />
         <IoSunny className="navIcon" title="Тема" />
       </div>
     </div>
